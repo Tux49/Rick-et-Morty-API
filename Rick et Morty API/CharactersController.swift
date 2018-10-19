4d1a5e0832d7c73c40bd3b6e03030ccd0bfc097d
Thierry Huu <huuth@macbook-pro-de-thierry.local>
2018-10-19 19:29:14 +0200

    Mise en place du projet

--- Rick et Morty API/CharactersController.swift
+++ Rick et Morty API/CharactersController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CharactersController.swift
 //  Rick et Morty API
 //
 //  Created by Thierry Huu on 19/10/2018.
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CharactersController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
