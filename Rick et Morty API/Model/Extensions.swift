58ce74b4de8bebc3e251b53419b0b1382bb3eb9c
Thierry Huu <huuth@macbook-pro-de-thierry.local>
2018-10-21 12:12:57 +0200

    UICollectionViewCell

---
+++ Rick et Morty API/Model/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  Rick et Morty API
+//
+//  Created by Thierry Huu on 21/10/2018.
+//  Copyright © 2018 Thierry Huu. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+}
---
+++ Rick et Morty API/View/CharacterCell.swift
@@ -0,0 +1,24 @@
+//
+//  CharacterCell.swift
+//  Rick et Morty API
+//
+//  Created by Thierry Huu on 21/10/2018.
+//  Copyright © 2018 Thierry Huu. All rights reserved.
+//
+
+import UIKit
+
+class CharacterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var holderView: UIView!
+    @IBOutlet weak var characterIV: UIImageView!
+    
+    var character: Character!
+    
+    func setupCell(_ character: Character) {
+        self.character = character
+        self.nameLbl.text = self.character.name
+        
+    }
+}
